90bd67638271b4185395b77719c54f7cd87f43ae
Diogo Luiz <dioluizsan@gmail.com>
2020-06-26 11:46:45 -0300

    tratando erros

--- xadrez-console/Tabuleiro/Tabuleiro.cs
+++ xadrez-console/Tabuleiro/Tabuleiro.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Buffers;
 using System.Collections.Generic;
 using System.Text;
 
+
 namespace tabuleiro {
     class Tabuleiro {
-        public int linhas{ get; set; }
+        public int linhas { get; set; }
         public int colunas { get; set; }
 
         private Peca[,] pecas;
@@ -12,15 +14,36 @@ namespace tabuleiro {
         public Tabuleiro(int linhas, int colunas) {
             this.linhas = linhas;
             this.colunas = colunas;
-            pecas = new Peca[linhas,colunas];
+            pecas = new Peca[linhas, colunas];
         }
-        public Peca peca(int linha,int coluna) {
+        public Peca peca(int linha, int coluna) {
             return pecas[linha, coluna];
         }
-
-        public void colocarPeca(Peca P,Posicao pos){
+        public Peca peca(Posicao pos) {
+            return pecas[pos.linha, pos.coluna];
+        }
+        public bool existePeca(Posicao pos) {
+            validarPosicao(pos);
+            return peca(pos) != null;
+        }
+        public void colocarPeca(Peca P, Posicao pos) {
+            if(existePeca(pos)){
+                throw new TabuleiroException("Ja existe peca nessa posição!");
+            }
             pecas[pos.linha, pos.coluna] = P;
             P.posicao = pos;
+        }
+
+        public bool posicaValida(Posicao pos) {
+            if (pos.linha < 0 || pos.linha >= linhas || pos.coluna < 0 || pos.coluna > colunas) {
+                return false;
+            }
+            return true;
+        }
+        public void validarPosicao(Posicao pos) {
+            if (!posicaValida(pos)) {
+                throw new TabuleiroException("Posicao invalida!");
+            }
         }
     }
 }
